2ccbccc4a4827dd015a3c1ea63c1f313a909429f
fmc127 <jonmccoy8072@gmail.com>
2020-03-30 10:57:16 -0400

    Generated scaffold. Checked all generated files

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@
 
 Rails.application.routes.draw do
   # RESTful routes
-  resources :examples, except: %i[new edit]
-
+  resources :posts, except: %i[new edit]
+  # Index
+  get '/posts' => 'posts#index'
+  # Show
+  get '/posts/:id' => 'posts#show'
+  # Create
+  post '/posts' => 'posts#create'
+  # Update
+  patch '/posts/:id' => 'posts#update'
+  # Destroy
+  delete '/posts/:id' => 'posts#destroy'
   # Custom routes
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
